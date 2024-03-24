d97633a504d46e059f72adef06ee9ff3d5c4d108
Andres Higuera <anfehibe@gmail.com>
2024-03-24 11:15:08 -0500

    fist commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: '',
+    component: DashboardComponent,
+   }
+
+];
